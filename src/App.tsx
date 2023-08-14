5f2f7fc700084f88169047be2f31f5ecd0eebb13
CN-7W9R8Y2\antony <antony.chen@liferay.com>
2023-08-14 10:13:22 +0800

    add img and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/logo.svg';
 import robots from './mockdata/robots.json';
 import Robot from './components/robot';
 //ts don't konw the css file, need add declaration in the *.d.ts. 
@@ -9,6 +9,10 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
+      <div className={styles.appHeader}>
+        <img src={logo} className={styles.appLogo} alt="logo"/>
+        <h1>This is a react project created by Antony</h1>
+      </div>
       <div className={styles.robotList}>
         {robots.map(r => (
           <Robot id={r.id} name={r.name} email={r.email} />
